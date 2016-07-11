d1f15ccf17858acf84b2092924c20edc6d3565d4
magui584 <magui584518@126.com>
2016-07-11 14:47:06 +0800

    qq

---
+++ 移动端/js/jianli.js
@@ -0,0 +1,69 @@
+
+FastClick.attach(document.body);
+
+~function () {
+    var winW = document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = winW / 750 * 100 + "px";
+}();
+
+
+new Swiper(".swiper-container", {
+    loop: true,
+    direction: "vertical",
+    onSlidePrevEnd: changeEnd,
+    onSlideNextEnd: changeEnd,
+});
+
+function changeEnd(swiper) {
+    var n = swiper.activeIndex,
+        slideAry = swiper.slides;
+    [].forEach.call(slideAry, function (slide, index) {
+        console.log(n)
+        if (n === index) {
+            if (n == 1) {
+                slide.id = 'fir'
+            } else if (n == 2) {
+                slide.id = 'two';
+            } else if (n == 3) {
+                slide.id = 'thr'
+            } else if (n == 4) {
+                slide.id = "foun"
+            } else if (n == 5) {
+                slide.id = "five"
+            } else if (n == 6) {
+                slide.id = "six"
+            }
+
+
+
+            if (slide.id) {
+                slide.style.opacity = '1';
+            }
+            return;
+        }
+
+        slide.id = null;
+
+    });
+
+}
+
+var music = document.getElementById("music"),
+    musicAudio = document.getElementById("musicAudio");
+window.setTimeout(function () {
+    musicAudio.play();
+    musicAudio.addEventListener("canplay", function () {
+        music.style.display = "block";
+        music.className = "music move";
+
+    }, false);
+}, 1000);
+music.addEventListener("click", function () {
+    if (musicAudio.paused) {
+        musicAudio.play();
+        music.className = "music move";
+        return;
+    }
+    musicAudio.pause();
+    music.className = "music";
+}, false);
